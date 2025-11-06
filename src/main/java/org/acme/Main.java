9d951b15d6a9bd03afa1e7ef1f7861c5f8797806
flamurjahiri <flamurjahiri@kodelabs.com>
2025-11-06 14:05:50 +0100

    string as reproducer

--- src/main/java/org/acme/Main.java
+++ src/main/java/org/acme/Main.java
@@ -3,53 +3,46 @@ package org.acme;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
-import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static org.acme.MockDataHelper.ENTITIES;
 
 public class Main {
+
+    public static final List<String> ENTITIES = IntStream.range(0, 5000).mapToObj(i -> "Entity " + i).collect(Collectors.toList());
 
 
     public static void main(String[] args) {
 
         var mergeFive = Multi.createFrom().iterable(ENTITIES)
-                .group().by(i -> i.ref.id).
-                onItem()
-                .transformToUni(r -> {
+                .group().by(i -> i.split(" ")[1])
+                .onItem().transformToUni(r -> {
 
                     System.out.printf("Emitting mergeFive item: %s\n", r.key());
 
                     return r.collect().in(HashMap::new, (a, b) -> {
-                        a.computeIfAbsent(b.id, k -> "Random generator " + r.key());
+                        a.computeIfAbsent(b, k -> "Random generator " + r.key());
                     });
 
-                }).merge(5)
-                .collect()
-                .last()
+                }).merge(5).collect().last()
 //                .ifNoItem()
 //                .after(Duration.ofSeconds(20))
 //                .failWith(new RuntimeException("could not emit all items items"))
-                .onItemOrFailure()
-                .invoke((i, f) -> System.out.printf("mergeFive %s\n", f == null ? "succeeded" : "failed with error: %s".formatted(f.getMessage())));
+                .onItemOrFailure().invoke((i, f) -> System.out.printf("mergeFive %s\n", f == null ? "succeeded" : "failed with error: %s".formatted(f.getMessage())));
 
 
         var mergeAll = Multi.createFrom().iterable(ENTITIES)
-                .group().by(i -> i.ref.id).
-                onItem()
-                .transformToUni(r -> {
+                .group().by(i -> i.split(" ")[1])
+                .onItem().transformToUni(r -> {
 
                     System.out.printf("Emitting mergeAll item: %s\n", r.key());
 
                     return r.collect().in(HashMap::new, (a, b) -> {
-                        a.computeIfAbsent(b.id, k -> "Random generator " + r.key());
-                    });
-
-                }).merge(Integer.MAX_VALUE)
-                .collect()
-                .last()
-                .onItemOrFailure()
-                .invoke((i, f) -> System.out.printf("mergeAll %s\n", f == null ? "succeeded" : "failed"));
+                        a.computeIfAbsent(b, k -> "Random generator " + r.key());
+            });
+        }).merge(Integer.MAX_VALUE).collect().last().onItemOrFailure().invoke((i, f) -> System.out.printf("mergeAll %s\n", f == null ? "succeeded" : "failed"));
 
 
         Uni.combine().all().unis(mergeFive, mergeAll).withUni((a, c) -> {
